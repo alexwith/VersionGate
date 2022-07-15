057c3fb4ec50f39dd3e6bfa5f1858e947ff0d829
Alex <alex@with.priv.no>
2022-07-15 16:14:20 +0200

    Fix handshake being read wrongly

--- standalone/src/main/java/net/versiongate/standalone/connection/ProtocolHandler.java
+++ standalone/src/main/java/net/versiongate/standalone/connection/ProtocolHandler.java
@@ -9,10 +9,14 @@ public interface ProtocolHandler {
     ProtocolHandler CLIENT = (context, packetId, payload) -> {
         final var state = context.getState();
         if (state == State.HANDSHAKE && packetId == 0) {
+            // we need to read the whole handshake to get the correct values
+            final int protocol = ProtocolUtils.readVarInt(payload);
+            final String address = ProtocolUtils.readString(payload, 255);
+            final short port = payload.getShort();
             final int stateId = ProtocolUtils.readVarInt(payload);
             final State nextState = switch (stateId) {
                 case 1 -> State.STATUS;
-                case 47 -> State.LOGIN;
+                case 2 -> State.LOGIN;
                 default -> throw new IllegalStateException("Unexpected value: " + stateId);
             };
             context.setState(nextState);
